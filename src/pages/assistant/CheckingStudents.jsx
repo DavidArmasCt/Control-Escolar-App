faaac9d1c309e2ee7df1b570f93f0d0aa3881b38
DavidArmasCt <154278621+DavidArmasCt@users.noreply.github.com>
2023-12-27 13:08:40 -0600

    Adding function to get students info from database

--- src/pages/assistant/CheckingStudents.jsx
+++ src/pages/assistant/CheckingStudents.jsx
@@ -1,8 +1,37 @@
-
-export const CheckingStudents = () => {
-  return (
+    import { useDispatch, useSelector } from "react-redux"
+    import { useEffect } from "react";
+    import { startStudentsList } from "../../store/assistant/thunks";
     
-      <h1>CheckingStudents</h1>
     
-  )
-}
+    
+    export const CheckingStudents = () => {
+    //Añadir funcion para listar los usuarios registrados  
+    
+    const dispatch = useDispatch();
+    
+      useEffect(() => {
+        // Invocamos la función para cargar la lista de usuarios al montar el componente
+        dispatch(startStudentsList());
+      }, [dispatch]);
+    
+      const users = useSelector((state) => state.assistant.users);
+    
+    
+      return (
+        <div>
+          <h2>Lista de Usuarios:</h2>
+          <ul>
+            {users.map((student) => (
+              <li key={student.id}>
+                {student.Name} {student.LastNameP} {student.LastNameM}
+                {/* Mostrar otros campos del usuario según sea necesario */}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )
+    }
+    
+    
+    
+    
--- src/store/assistant/thunks.js
+++ src/store/assistant/thunks.js
@@ -11,3 +11,23 @@ export const startSavingStudent = (formData, route) => {
         console.log(newRoute, formData)
     }
 }
+
+export const startStudentsList = ()=>{
+    return async (dispatch) => {
+        try {
+          const usuariosCollection = collection(FirebaseDB, '/Control-Escolar-DB/Usuarios/Estudiantes');
+    
+          const querySnapshot = await getDocs(usuariosCollection);
+          const students = [];
+    
+          querySnapshot.forEach((doc) => {
+            students.push({ id: doc.id, ...doc.data() });
+          });
+    
+          dispatch(gettingStudents(students));
+        } catch (error) {
+          console.error('Error al cargar usuarios:', error);
+          // Manejo de errores, despacho de acciones adicionales si es necesario
+        }
+      };
+}

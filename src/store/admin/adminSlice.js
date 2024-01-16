e3517d82c2e4d67f16fd22a6e0689354745e37a3
DavidArmasCt <154278621+DavidArmasCt@users.noreply.github.com>
2024-01-16 17:41:51 -0600

    Complements on Slices and Store

--- src/store/admin/adminSlice.js
+++ src/store/admin/adminSlice.js
@@ -5,17 +5,28 @@ export const adminSlice = createSlice({
     initialState: {
         CurrentAcademicYear: null,
         CurrentPeriod: null,
+        Subjects: [],
+        Grades: [],
+        CurrentGeneration: null,
     },
     reducers: {
+       getAcademicInfo: (state, action)=>{
+        state.CurrentAcademicYear = action.CurrentAcademicYear;
+        state.CurrentPeriod = action.CurrentPeriod;
+       }, 
        setPeriod: (state, action)=>{
         state.CurrentPeriod = action.payload.CurrentPeriod;
        },
-       setNewAcademicYear: ()=>{
+       setNewAcademicYear: (state, action)=>{
         state.CurrentAcademicYear = action.payload.CurrentAcademicYear;
+       },
+       setSubjects: (state, action) => {
+        state.Subjects = action.payload.Subjects;
        }
-    }   
+    }
+
 });
 
 
 // Action creators are generated for each case reducer function
-export const { setPeriod, setNewAcademicYear } = adminSlice.actions;
+export const { setPeriod, setNewAcademicYear, setSubjects } = adminSlice.actions;

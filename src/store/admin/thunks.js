7534ee79f6392fa181a5bf07cf0afb2c253f88dd
DavidArmasCt <154278621+DavidArmasCt@users.noreply.github.com>
2024-01-16 18:09:40 -0600

    Creating todo's for next Thunks

---
+++ src/store/admin/thunks.js
@@ -0,0 +1,17 @@
+export const getAcademicInfo = ()=>{
+    
+}
+
+export const setAcademicInfo = ()=>{
+    return
+    
+}
+
+export const endingSchoolYear=(currentYear)=>{
+    return currentYear;
+}
+
+export const setNewAcademicYear= ()=>{
+    const NewAcademicYear = "";
+    return NewAcademicYear;
+}

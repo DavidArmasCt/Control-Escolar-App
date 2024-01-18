5204a70b50725c48fb44144423fd9d9debfcf4f6
DavidArmasCt <154278621+DavidArmasCt@users.noreply.github.com>
2024-01-18 11:26:41 -0600

    Definition of uncoming features

--- src/store/admin/thunks.js
+++ src/store/admin/thunks.js
@@ -5,16 +5,13 @@ import { getInitialAcademicInfo } from './adminSlice'
 export const getAcademicInfo = ()=>{
     return async (dispatch) => {
       try{
-        /*const docReference = collection(FirebaseDB,'/Control-Escolar-DB/Informacion-Curricular/Ciclo-Escolar/')
-        const academicData = await getDocs(docReference, where('Estado-Ciclo','==','1' ));*/
-
         const querySnapshot = await getDocs(query(collection(FirebaseDB,'/Control-Escolar-DB/Informacion-Curricular/Administracion-Escolar/'),where('Active', '==', true)));
         
-
         querySnapshot.forEach((doc) => {
           const academicInfo = doc.data();
           dispatch(getInitialAcademicInfo(academicInfo));
         });
+
         if (querySnapshot.empty) {
           console.log('No se encontraron datos que cumplan con la condición');
           // Puedes manejar el caso en que no se encuentren datos que cumplan con la condición.
@@ -23,14 +20,25 @@ export const getAcademicInfo = ()=>{
         console.error('Error en el resultado de la consulta', error)
       }    
     }
-}
+}//Funcion que obtiene los datos basicos necesarios para consulta y registro de datos
 
 export const setAcademicInfo = ()=>{
+  /*
+    TODOs:
+    ->Crea nuevo ciclo escolar y mapa curricular
+  */
     return
     
 }
 
 export const endingSchoolYear=(currentYear)=>{
+    /*
+      ->Cambiar Status a False
+      ->Cambiar de grado a todos los alumnos que tengan promedio aprobatorio y cuyo grado sea diferente de 3 o bien el máximo numero de grados existentes
+      ->Alumnos de grado máximo cambiar status a "Graduado" o su equivalente
+      ->Retener alumnos con grado reprobatorio
+      ->
+    */
     return currentYear;
 }
 

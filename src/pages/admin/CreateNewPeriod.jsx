61780e31357a80778eea77e53056017a259a8d99
DavidArmasCt <154278621+DavidArmasCt@users.noreply.github.com>
2024-01-25 23:39:08 -0600

    New todo's for up´coming features

--- src/pages/admin/CreateNewPeriod.jsx
+++ src/pages/admin/CreateNewPeriod.jsx
@@ -10,13 +10,22 @@ export const CreateNewPeriod = () => {
     dispatch(getFormAproval())
   }, [dispatch])
   
+  const writtingValidation = useSelector((state) => state.admin.FormApproved)
   
-  
+  /*
+    ->Ingresa la nueva generación > Ingresa los datos de la generacion > Ingresa la cantidad de grados > Ingresa la cantidad de grupos > Verifica los datos > PLANCHADO DE DATOS
+   */
   
   
   return (
-    <div>
-      <h1>HOLA</h1>
+    <div className="contentForm">
+      {writtingValidation ? 
+      (
+        <h1>Comienza el formulario de generación de nuevo ciclo escolar</h1>
+      ):
+      (
+        <h1>Existe un ciclo previamente abierto</h1>
+      )}
     </div>
   )
 }
